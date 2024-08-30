31dfb5f919991a5e80411f3364a338d797d03ea2
kor <kor@dwarshuis.com>
2024-08-30 10:38:23 +0200

    Generated data

---
+++ output/xrefs-history/xrefs-data-1724962712813.js
@@ -0,0 +1,16 @@
+const allXrefs = {
+  "xrefs": [
+    {
+      "externalSpec": "test-1",
+      "term": "Aal",
+      "repoUrl": "https://github.com/blockchainbird/spec-up-xref-test-1",
+      "terms_dir": "spec/term-definitions",
+      "owner": "blockchainbird",
+      "repo": "spec-up-xref-test-1",
+      "site": "https://blockchainbird.github.io/spec-up-xref-test-1/",
+      "commitHash": [
+        "f66951f1d378490289caab9c51141b44a0438365"
+      ]
+    }
+  ]
+};
