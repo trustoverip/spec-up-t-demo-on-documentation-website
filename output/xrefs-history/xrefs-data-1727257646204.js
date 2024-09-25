9662aff1c1a9ed82729827a48687b6969fe911bc
kor <kor@dwarshuis.com>
2024-09-25 12:20:39 +0200

    Generated

---
+++ output/xrefs-history/xrefs-data-1727257646204.js
@@ -0,0 +1,26 @@
+const allXrefs = {
+  "xrefs": [
+    {
+      "externalSpec": "test-1",
+      "term": "Aal",
+      "repoUrl": "https://github.com/blockchainbird/spec-up-xref-test-1",
+      "terms_dir": "spec/term-definitions",
+      "owner": "blockchainbird",
+      "repo": "spec-up-xref-test-1",
+      "site": "https://blockchainbird.github.io/spec-up-xref-test-1/",
+      "commitHash": "f1fa28471b969b613587cf1be7701c9e168be866",
+      "content": "[[def: AAL]]:\n\n~ See: [[ref: authenticator assurance level]].\n\n~ This is an addition, for testing purposes.\n\n~ Another addition for testing.\n\n~ Third addition\n\n~ Fourth addition\n\n~ Another edit, showing that an xref refers to the original version.\n"
+    },
+    {
+      "externalSpec": "does-not-exist",
+      "term": "Foo",
+      "repoUrl": null,
+      "terms_dir": null,
+      "owner": null,
+      "repo": null,
+      "site": null,
+      "commitHash": null,
+      "content": null
+    }
+  ]
+};
