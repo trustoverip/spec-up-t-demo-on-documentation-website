e0bcfbc96b7a63e74c305d906761e4e74a1cfd0e
Kor Dwarshuis <proximus@fastmail.com>
2025-03-31 19:29:59 +0200

    Lots of updates

--- output/xtrefs-history/xtrefs-data-1743441959224.js
+++ output/xtrefs-history/xtrefs-data-1743441959224.js
@@ -23,12 +23,12 @@ const allXTrefs = {
       "content": "This term was not found in the external repository."
     },
     {
-      "externalSpec": "test-1",
-      "term": "AAL",
-      "repoUrl": "https://github.com/blockchainbird/spec-up-xref-test-1",
-      "terms_dir": "spec/term-definitions",
-      "owner": "blockchainbird",
-      "repo": "spec-up-xref-test-1",
+      "externalSpec": "test-3",
+      "term": "Scope",
+      "repoUrl": "https://github.com/trustoverip/ctwg-main-glossary",
+      "terms_dir": "spec/terms-definitions",
+      "owner": "trustoverip",
+      "repo": "ctwg-main-glossary",
       "site": null,
       "commitHash": "not found",
       "content": "This term was not found in the external repository."
